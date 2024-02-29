94b98d9ee8733ad9870d1990bad61d4536e76dba
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-02-28 17:30:40 -0800

    < completed >

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# froyo-orders
+# Froyo Order
+
+## Description
+In this project, you will be building out the functionality of a simple app that asks a user for some numbers and then calculates some statistics according to the user's input.
+
+## Submission
+
+Submit a link to your Github repo.
+
+## Technology
+
+-HTML
+-CSS
+-JavaScript
+
+## Author
+
+Daniel San Pedro
---
+++ index.js
@@ -0,0 +1,18 @@
+//Create prompt asking user to input froyo flavors
+//userInputString
+const froyoFlavors = prompt(
+  "Enter froyo flavors! Separate them by commas.",
+  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+);
+// Split input into array
+const order = froyoFlavors.split(",");
+
+//Create Object from array
+const flavorCount = {};
+
+//Add flavors together
+for (const flavor of order) {
+  flavorCount[flavor] = flavorCount[flavor] ? flavorCount[flavor] + 1 : 1;
+}
+
+console.table(flavorCount);
